54bcf3916f07e056cee1425e78a40d10c590e533
ocean-love <love@oceanobservations.com>
2014-07-09 16:29:02 +0200

    Initial Commit

---
+++ React/Script/App/Components/Header.react.js
@@ -0,0 +1,39 @@
+/** @jsx React.DOM */ 
+var React = require('react');
+var TodoActions = require('../actions/TodoActions');
+var TodoTextInput = require('./TodoTextInput.react');
+
+var Header = React.createClass({
+
+  /**
+   * @return {object}
+   */
+  render: function() {
+    return (
+      <header id="header">
+        <h1>Mina Todoz</h1>
+        <TodoTextInput
+          id="new-todo"
+          placeholder="Vad ska du göra?"
+          onSave={this._onSave}
+        />
+      </header>
+    );
+  },
+
+  /**
+   * Event handler called within TodoTextInput.
+   * Defining this here allows TodoTextInput to be used in multiple places
+   * in different ways.
+   * @param {string} text
+   */
+  _onSave: function(text) {
+    if (text.trim()){
+      TodoActions.create(text);
+    }
+
+  }
+
+});
+
+module.exports = Header;
