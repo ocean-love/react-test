744212d6af142d568e06eaa94d396a0cf804370a
Niklas Sandler <niklas@oceanobservations.com>
2014-07-15 11:30:58 +0200

    Cleaned up and added comments. Also fixed focused

--- React/Script/App/Components/AnimatedMenu.react.js
+++ React/Script/App/Components/AnimatedMenu.react.js
@@ -1,13 +1,15 @@
 /** @jsx React.DOM */ 
 var React = require('react/addons');
-
-//var ReactTransitionGroup  = React.addons.CSSTransitionGroup;
 var ReactTransitionGroup  = React.addons.TransitionGroup;
-
-
 var $ = require('jquery-browserify');
 
 var MenuItem = React.createClass({
+     getInitialState: function(){
+        return { 
+            focused: false
+        };
+  },
+    // Called when component is added to an exisiting TransitionGroup
     componentWillEnter: function(callback) {
         var $el = $(this.getDOMNode());
         $el
@@ -15,21 +17,21 @@ var MenuItem = React.createClass({
             .css({opacity:1})
             .animate({"opacity": "0"}, 1000, callback);
     },
+    // Callback for WillEnter
     componentDidEnter: function() {
         var $el = $(this.getDOMNode());
         $el
             .stop(true)
             .animate({"opacity": "1"}, 1000);
     },
-    clickHandle: function(index){
+    // Handles clicks for the individual MenuItems
+    clickHandler: function(){
         /// The click handler will update the state with
         /// the index of the focused menu entry       
  
         //Animate selected item using jQuery
-        //        var elem =  $(this.refs["menuItem_"+index].getDOMNode())
-        var elem =  $(this.getDOMNode())
-
-        elem
+        var $el =  $(this.getDOMNode());
+        $el
           .stop()
           /// Animate left margin to push selected item...
           .animate({"marginLeft": "500px"}, 1000, function(){  
@@ -38,35 +40,24 @@ var MenuItem = React.createClass({
           });
 
         /// Pass event to parent
-        this.props.onClick(this);       
-        
+        this.props.onClick(this);
+
     },
     render: function() {
-        return <span onClick={this.clickHandle}>{this.props.children}</span>;
+        return <span className={this.props.className} onClick={this.clickHandler}>{this.props.children}</span>;
     }
 });
 
 
 var Menu = React.createClass({
-    componentWillEnter: function(cb) {
-        console.log("TEST1");
-        //var $el = $(this.getDOMNode());
-        //var height = $el[0].scrollHeight;
-        //$el.stop(true).height(0).animate({height:height}, 200, cb);
-    },
-
-  componentDidMount: function(){
-    console.log("component did mount");
-  },
-  componentDidUpdate: function(){
-    console.log("component did update");
-  },
+  // Init component
   getInitialState: function(){
-        return {focused: 0, menuItems: ['hello', 'world', 'click', 'me']};
-  },
-  onChildEnter: function(index){
-      var elem =  $(this.refs["menuItem_"+index].getDOMNode());
-      console.log("ON enter");
+        return { 
+            // Start of with the first item focused
+            focused: 0,
+            // Starting menu items
+            menuItems: ['hello', 'world', 'click', 'me']
+        };
   },
   clickHandler: function(index){
       /// Update state using React
@@ -94,10 +85,13 @@ var Menu = React.createClass({
         { self.state.menuItems.map(function(m, index){
             var style = '';
             if(self.state.focused == index){
+                console.log("style = focused");
                 style = 'focused';
             }
             var refName = "menuItem_"+index;
 
+            // Render as manu MenuItem components as we have in the map
+            // The key value is accessible as props.children in the MenuItem component, same as className
             return <MenuItem
                 onClick={self.clickHandler.bind(null,index)}
                 key={"menu_item"+index}
